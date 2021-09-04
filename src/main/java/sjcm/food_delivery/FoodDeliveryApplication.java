aed5b1194cccd4e513f53e2202863835affed88a
sejun <pkt369@naver.com>
2021-09-04 18:43:40 +0900

    배달의 민족 클론코딩 초기세팅 완료

---
+++ src/main/java/sjcm/food_delivery/FoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package sjcm.food_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApplication.class, args);
+	}
+
+}
